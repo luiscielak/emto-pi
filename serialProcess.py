bd1e7f3144231ab5bb8e40f100f652d5fcf88ed4
Luis Cielak <luis.cielak@gmail.com>
2014-01-27 16:49:17 +0000

    add python server files

---
+++ serialProcess.py
@@ -0,0 +1,42 @@
+import serial
+import time
+import multiprocessing
+ 
+class SerialProcess(multiprocessing.Process):
+ 
+    def __init__(self, taskQ, resultQ):
+        multiprocessing.Process.__init__(self)
+        self.taskQ = taskQ
+        self.resultQ = resultQ
+        self.usbPort = '/dev/ttyACM0'
+        self.sp = serial.Serial(self.usbPort, 115200, timeout=1)
+ 
+    def close(self):
+        self.sp.close()
+ 
+    def sendData(self, data):
+        print "sendData start..."
+        self.sp.write(data)
+        time.sleep(3)
+        print "sendData done: " + data
+ 
+    def run(self):
+ 
+    	self.sp.flushInput()
+ 
+        while True:
+            # look for incoming tornado request
+            if not self.taskQ.empty():
+                task = self.taskQ.get()
+ 
+                # send it to the arduino
+                self.sp.write(task + "\n");
+                print "arduino received from tornado: " + task
+ 
+            # look for incoming serial data
+            if (self.sp.inWaiting() > 0):
+            	result = self.sp.readline().replace("\n", "")
+ 
+                # send it back to tornado
+            	self.resultQ.put(result)
+
