4355ec3fe24aaf6b0ec22c54718dcd4a7c6aa789
Luis Cielak <luis.cielak@gmail.com>
2014-04-04 14:21:46 -0400

    Revert "set neopixel color based on in-browser selection"
    This reverts commit c55afc45ddd18e999c225e7e23800e40c206de8d.

--- totem/pixelPi/pixelPi.ino
+++ totem/pixelPi/pixelPi.ino
@@ -1,120 +1,42 @@
-#include <Adafruit_NeoPixel.h>
-
-#define PIN 6
-
 #define LED_PIN 13
-
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(60, PIN, NEO_GRB + NEO_KHZ800);
-
+ 
 int msgByte= -1;         // incoming byte
 const int msgSize = 50;  // max message size
 char msgArray[msgSize];  // array for incoming data
 int msgPos = 0;          // current position
-
+ 
 void setup() {
-  Serial.begin(115200);
-  pinMode(LED_PIN, OUTPUT);
-
-  strip.begin();
-  strip.show(); // Initialize all pixels to 'off'
+    Serial.begin(115200);
+    pinMode(LED_PIN, OUTPUT);
 }
-
+ 
 void loop() {
-  handleSerial();
-
+    handleSerial();
 }
-
+ 
 void handleSerial() {  
   if (Serial.available() > 0) {
-    digitalWrite(LED_PIN, HIGH);
-
+  	digitalWrite(LED_PIN, HIGH);
     msgByte = Serial.read();
-
+ 
     if (msgByte != '\n') {
       // add incoming byte to array
       msgArray[msgPos] = msgByte;
       msgPos++;
-    } 
-    else {
+    } else {
       // reached end of line
       msgArray[msgPos] = 0;
-
-
-
-
+ 
       // here the message is processed
-      setColor(msgArray);
-
-      // send it back over serial
+      // for now just send it back over serial
       Serial.println(msgArray);
-
+ 
       // reset byte array
       for (int c = 0; c < msgSize; c++) 
         msgArray[c] = ' ';
-
+ 
       msgPos = 0;
       digitalWrite(LED_PIN, LOW);
     }
   }
 }
-
-void setColor() {
-  colorWipe(strip.Color(255,255,255), 20); // White
-  colorWipe(strip.Color(0, 0, 0), 30); // Black
-
-}
-
-
-void setColor(char* c) {
-
-
-  int val = atoi(c);
-
-
-  switch (val) {
-  case 1:
-    colorWipe(strip.Color(0,0,255), 10); // Blue
-    colorWipe(strip.Color(0, 0, 0), 1); // Black
-    break;
-  case 2:
-    colorWipe(strip.Color(0,255,0), 10); // Green
-    colorWipe(strip.Color(0, 0, 0), 1); // Black
-    break;
-  case 3:
-    colorWipe(strip.Color(255,0,0), 10); // Red
-    colorWipe(strip.Color(0, 0, 0), 1); // Black
-    break;
-  case 4:
-    colorWipe(strip.Color(223, 0, 255), 10); // Purple
-    colorWipe(strip.Color(0, 0, 0), 1); // Black
-    break;    
-//  default: 
-//    setColor();
-  }
-
-
-
-}
-
-
-// Fill the dots one after the other with a color
-void colorWipe(uint32_t c, uint8_t wait) {
-  for(uint16_t i=0; i<strip.numPixels(); i++) {
-    strip.setPixelColor(i, c);
-    strip.show();
-    delay(wait);
-  }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
